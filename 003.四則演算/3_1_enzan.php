1979ec59c2fa936a864426fc3969680473816f74
furuyatakayuki <t.furuya0203@gmail.com>
2017-06-21 16:54:02 +0900

    四則演算 変数の宣言と表示

---
+++ 003.四則演算/3_1_enzan.php
@@ -0,0 +1,11 @@
+<?php
+
+const HUNDRED = 100;
+$x = 50;
+
+echo HUNDRED + $x . "<br>";
+echo HUNDRED - $x . "<br>";
+echo HUNDRED * $x . "<br>";
+echo HUNDRED / $x . "<br>";
+
+echo HUNDRED % $x . "<br>";
