85578d254489feb072d07adf99d8fecc96839f11
furuyatakayuki <t.furuya0203@gmail.com>
2017-06-22 13:06:43 +0900

    ループ処理 for文

---
+++ 008.ループ処理/8_2_for2.php
@@ -0,0 +1,9 @@
+<?php
+
+$str = "";
+
+for($i=0 ; $i < 30 ; $i++){
+    $str .= "A";
+}
+
+echo $str;
---
+++ 008.ループ処理/8_3_for3.php
@@ -0,0 +1,10 @@
+<?php
+
+$sum = 0;
+
+for($i=0 ; $i <= 100 ; $i++){
+    $sum += $i;
+    echo $sum . "<br>";
+}
+
+echo "<br>計算結果：" . $sum;
