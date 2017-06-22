a3285815ce80c9263822faee47120455f6b40420
furuyatakayuki <t.furuya0203@gmail.com>
2017-06-22 11:34:38 +0900

    条件分岐 switch文

---
+++ 005.条件分岐/5_1_switch1.php
@@ -0,0 +1,17 @@
+<?php
+
+$n = 1;
+
+switch ($n) {
+    case 1:
+        echo "one";
+        break;
+    
+    case 2:
+        echo "two";
+        break;
+    
+    default:
+        echo "想定外";
+        break;
+}
---
+++ 005.条件分岐/5_1_switch2.php
@@ -0,0 +1,16 @@
+<?php
+
+$ch = "A";
+
+switch ($ch) {
+    case 'A':
+        echo "英語";
+        break;
+    
+    case 'あ':
+        echo "日本語";
+        break;
+    
+    default:
+        break;
+}
