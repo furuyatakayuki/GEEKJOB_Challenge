60a0ec89106ab033e7ece90b41d6b0cc524b7677
furuyatakayuki <t.furuya0203@gmail.com>
2017-06-26 13:57:47 +0900

    ユーザ定義関数

---
+++ 009.関数/9_1_func.php
@@ -0,0 +1,12 @@
+<?php
+
+function prf(){
+    echo "name: FURUYA Takayuki<br>";
+    echo "birthday: 02/03<br>";
+    echo "Hello World!!<br>";
+}
+
+for($i=0 ; $i < 10 ; $i++){
+    prf();
+    echo "<br>";
+}
