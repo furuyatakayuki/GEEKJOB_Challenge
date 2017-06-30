9a32f606b51d88951bfcf74b686e7c121037d0f2
furuyatakayuki <t.furuya0203@gmail.com>
2017-06-30 14:09:42 +0900

    全情報の取得と表示

---
+++ 013.DB操作/03_pdo_show.php
@@ -0,0 +1,38 @@
+<table border="1" cellpadding="5">
+
+<?php
+
+// データベース接続用。ユーザ定義関数のmysql_acces()によりpdoオブジェクトを取得。
+require_once './00_mysql_acces.php';
+
+$sql = 'select * from profiles;';
+
+$pdo_obj = mysql_acces();
+
+$pdo_st = $pdo_obj->prepare($sql);
+
+$pdo_st->execute();
+
+$datas = $pdo_st->fetchAll(PDO::FETCH_ASSOC);
+
+$lim = count($datas[0]);
+
+echo "<tr>";
+foreach ($datas[0] as $key => $value) {
+    echo "<td>" . $key . "</td>";
+}
+echo "</tr>";
+
+foreach ($datas as $prf) {
+    echo "<tr>";
+    foreach ($prf as $key => $value) {
+        echo "<td>" . $value . "</td>";
+    }
+    echo "</tr>";
+}
+
+$pdo_obj = null;
+
+?>
+
+</table>
